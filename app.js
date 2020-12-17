d25693443c126e3c45d1ec14a2eb7ed4fbbb20cb
Manuel Garcia F <mgarciafrigo@gmail.com>
2020-12-17 12:22:32 -0300

    change sass config to use new style

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.use(
   sassMiddleware({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
-    indentedSyntax: true, // true = .sass and false = .scss
+    indentedSyntax: false,
     sourceMap: true,
-  }),
+  })
 );
 app.use(express.static(path.join(__dirname, 'public')));
 
